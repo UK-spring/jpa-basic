28b9f2b701b230e161d09afa1bbbbda3e3d4d77f
UK <hwangwonuk94@gmail.com>
2024-11-07 04:17:44 +0900

    feat: Tutor JPA Entity

--- src/main/java/org/entity/Tutor.java
+++ src/main/java/org/entity/Tutor.java
@@ -1,8 +1,21 @@
 package org.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tutor")
 public class Tutor {
 
+    @Id
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
+
+    @Column(name = "age", nullable = false)
     private Integer age;
 
     public Tutor(String name, Integer age) {
