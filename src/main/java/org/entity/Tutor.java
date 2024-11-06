a6b48a3b06fdf4e3af75e88e2ca0b74f34678138
UK <hwangwonuk94@gmail.com>
2024-11-07 03:58:42 +0900

    feat: Tutor Entity

---
+++ src/main/java/org/entity/Tutor.java
@@ -0,0 +1,13 @@
+package org.entity;
+
+public class Tutor {
+
+    private String name;
+    private Integer age;
+
+    public Tutor(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
